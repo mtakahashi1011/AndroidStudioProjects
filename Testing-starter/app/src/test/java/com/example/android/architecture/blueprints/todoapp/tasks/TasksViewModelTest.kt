7760e0c32f5f24205799f46a6e58bd336eee98be
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-06-28 17:48:58 +0900

    Create the first test and ViewModel tests

--- Testing-starter/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ Testing-starter/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,11 +1,46 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import android.media.metrics.Event
+import android.service.autofill.FillEventHistory
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.Matchers.`is`
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
+@Config(sdk = [30]) // Remove when Robolectric supports SDK 31
+@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
-        
+        tasksViewModel.addNewTask()
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+        assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
     }
 }
