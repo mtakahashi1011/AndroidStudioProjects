e2d8d7d5bf714b30035e6f1dc2b410b4b022b982
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-07-05 16:07:05 +0900

    Create Fake Repository and Fragment Test

--- Testing-starter/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ Testing-starter/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -6,6 +6,8 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import getOrAwaitValue
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.CoreMatchers.nullValue
@@ -18,8 +20,10 @@ import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
 @Config(sdk = [30]) // Remove when Robolectric supports SDK 31
-@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+    // Use a fake repository to be injected into the viewmodel
+    private lateinit var tasksRepository: FakeTestRepository
+
     private lateinit var tasksViewModel: TasksViewModel
 
     @get:Rule
@@ -27,7 +31,15 @@ class TasksViewModelTest {
 
     @Before
     fun setupViewModel() {
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        // We initialise the tasks to 3, with one active and two completed
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository.addTasks(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
+
     }
 
 
